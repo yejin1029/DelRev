7392db78302b406a281b06f8f691bfe25a4040a4
LeeBellHa <jongha8273@naver.com>
2025-06-03 09:09:12 +0900

    gameover

--- DelRev/Assets/1.Script/StartGame/GameOverUI.cs
+++ DelRev/Assets/1.Script/StartGame/GameOverUI.cs
@@ -1,15 +1,28 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOverUI : MonoBehaviour
 {
-  public void RestartGame()
-  {
-    Vector3 spawnPos = new Vector3(-2.5f, 2f, 45f);
-    transform.position = spawnPos;
+    public void RestartGame()
+    {
+        // 씬 로드 완료 시 콜백 등록
+        SceneManager.sceneLoaded += OnCompanySceneLoaded;
+        SceneManager.LoadScene("Company");
+    }
 
-    SceneManager.LoadScene("Company");
-  }
+    private void OnCompanySceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "Company")
+        {
+            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+            foreach (GameObject player in players)
+            {
+                player.transform.position = new Vector3(-4.5f, 2f, 45f);
+            }
+
+            // 한 번만 실행되도록 제거
+            SceneManager.sceneLoaded -= OnCompanySceneLoaded;
+        }
+    }
 }
