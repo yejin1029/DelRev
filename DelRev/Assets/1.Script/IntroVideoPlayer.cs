419f27ca4fa7e50a3a353ff4209ecf60f7172466
yejin1029 <dpwlsdl1029@gmail.com>
2025-12-03 01:36:26 +0900

    Add Intro

---
+++ DelRev/Assets/1.Script/IntroVideoPlayer.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+public class IntroVideoPlayer : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public string nextSceneName = "Company";
+
+    private void Start()
+    {
+        if (videoPlayer == null)
+            videoPlayer = GetComponent<VideoPlayer>();
+
+        // 영상 끝날 때 호출될 콜백 등록
+        videoPlayer.loopPointReached += OnVideoEnd;
+        videoPlayer.Play();
+    }
+
+    private void OnDestroy()
+    {
+        if (videoPlayer != null)
+            videoPlayer.loopPointReached -= OnVideoEnd;
+    }
+
+    private void OnVideoEnd(VideoPlayer vp)
+    {
+        // 영상 끝나면 로딩씬 거쳐서 Company로
+        SceneLoader.Load(nextSceneName);
+    }
+
+    // 스킵 버튼에 연결할 함수 (선택)
+    public void Skip()
+    {
+        OnVideoEnd(videoPlayer);
+    }
+}
